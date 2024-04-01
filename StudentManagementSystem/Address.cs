0e67a5e3a45f5c1aacabed74c0fa662e6ade040b
Anchal Kumar Minz <anchalminz71@gmail.com>
2024-04-01 08:00:42 +0530

    project-01

---
+++ StudentManagementSystem/Address.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem
+{
+    internal class Address
+    {
+        private string _addressLine1;
+        private string _post;
+        private string _block;
+        private string _district;
+        private string _state;
+        private int _pinCode;
+
+        public string _AddressLine1
+        { 
+            get { return _addressLine1; }
+            set { _addressLine1 = value; }
+        }
+        public string _Post
+        {
+            get { return _post; }
+            set { _post = value; }
+
+        }
+       
+        public string _Block
+        {
+            get { return _block; }
+            set { _block = value; }
+        }
+
+        public string _District
+        {
+            get { return _district; }
+            set { _district = value; }
+        }
+
+        public string _State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+
+        public int _PinCode
+        {
+            get { return _pinCode; }
+            set { _pinCode = value; }
+        }
+
+
+    }
+}
---
+++ StudentManagementSystem/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem
+{
+    internal class Student
+    {
+        private string _firstName;
+        private string _middleName;
+        private string _lastName;
+        private int _age;
+        private int _rollNumber;
+        private Address _address;
+        private string _standard;
+        private string _subject;
+        private string _hobbies;
+        private DateTime _dateTime;
+
+    }
+}
